8803bf13eff0166ee5c1ad6b3400568028f9192c
unknown <Jayalakshmiraj@gmail.com>
2019-02-28 11:56:47 +0530

    angularMaterial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewLoginComponent } from './new-login/new-login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{
+  path:"login",
+  component:LoginComponent
+},
+{
+  path:"dashboard",
+  component:DashboardComponent
+},
+{
+  path:"new-login",
+  component:NewLoginComponent
+},
+{
+  path:"",
+  redirectTo:"./new-login",
+  pathMatch:"full"
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCardModule} from '@angular/material/card';
+import { NewComponent } from './new/new.component';
+import { NewLoginComponent } from './new-login/new-login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    NewComponent,
+    NewLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule,
+    MatCardModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
